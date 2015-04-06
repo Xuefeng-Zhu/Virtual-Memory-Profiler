ad5d2a2617c464af8f79bea0f836d43c7be50b69
CS 423 <xzhu15@illinois.edu>
2015-04-05 21:13:50 -0500

    upload data for n = 7

--- multiprogramming.sh
+++ multiprogramming.sh
@@ -5,7 +5,3 @@ nice ./work 200 R 10000 &
 nice ./work 200 R 10000 &
 nice ./work 200 R 10000 &
 nice ./work 200 R 10000 &
-nice ./work 200 R 10000 &
-nice ./work 200 R 10000 &
-nice ./work 200 R 10000 &
-nice ./work 200 R 10000 &
